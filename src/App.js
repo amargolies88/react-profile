cc65963f36ac92904a6209d473056158af920634
Alan Margolies <amargolies88@gmail.com>
2020-04-19 13:29:22 -0400

    yes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import Header from './components/Header';
-import About from './pages/About';
 import Footer from './components/Footer';
 import Page from './components/Page';
 import './App.css';
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,23 +6,23 @@ function Nav() {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <button className="navbar-brand" href="#">Alan Margolies</button>
+            <button className="navbar-brand" onClick={() => dispatch({ page: "about" })}>Alan Margolies</button>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" onClick={() => dispatch({ page: "about" })}>About <span className="sr-only">(current)</span></a>
+                        <button className="nav-link" onClick={() => dispatch({ page: "about" })}>About <span className="sr-only">(current)</span></button>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" onClick={() => dispatch({ page: "portfolio" })}>Portfolio <span className="sr-only">(current)</span></a>
+                        <button className="nav-link" onClick={() => dispatch({ page: "portfolio" })}>Portfolio <span className="sr-only">(current)</span></button>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" onClick={() => dispatch({ page: "contact" })}>Contact <span className="sr-only">(current)</span></a>
+                        <button className="nav-link" onClick={() => dispatch({ page: "contact" })}>Contact <span className="sr-only">(current)</span></button>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" onClick={() => dispatch({ page: "resume" })}>Resume <span className="sr-only">(current)</span></a>
+                        <button className="nav-link" onClick={() => dispatch({ page: "resume" })}>Resume <span className="sr-only">(current)</span></button>
                     </li>
                 </ul>
             </div>
