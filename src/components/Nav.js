bd5d0b8d013fcaae6d681d866005e0a469546413
Alan Margolies <amargolies88@gmail.com>
2020-05-15 11:55:47 -0400

    added new color scheme, button animation, and working resume link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,28 +5,29 @@ function Nav() {
     const [state, dispatch] = usePageContext();
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <button className="navbar-brand" onClick={() => dispatch({ page: "about" })}>Alan Margolies</button>
+        <nav className="navbar navbar-expand-lg">
+            <button className="btn navbar-brand" onClick={() => dispatch({ page: "about" })}>Alan Margolies</button>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <button className="nav-link" onClick={() => dispatch({ page: "about" })}>About <span className="sr-only">(current)</span></button>
+                        <button className="btn nav-link" onClick={() => dispatch({ page: "about" })}>About <span className="sr-only">(current)</span></button>
                     </li>
                     <li className="nav-item active">
-                        <button className="nav-link" onClick={() => dispatch({ page: "portfolio" })}>Portfolio <span className="sr-only">(current)</span></button>
+                        <button className="btn nav-link" onClick={() => dispatch({ page: "portfolio" })}>Portfolio <span className="sr-only">(current)</span></button>
                     </li>
                     <li className="nav-item active">
-                        <button className="nav-link" onClick={() => dispatch({ page: "contact" })}>Contact <span className="sr-only">(current)</span></button>
+                        <button className="btn nav-link" onClick={() => dispatch({ page: "contact" })}>Contact <span className="sr-only">(current)</span></button>
                     </li>
                     <li className="nav-item active">
-                        <button className="nav-link" onClick={() => dispatch({ page: "resume" })}>Resume <span className="sr-only">(current)</span></button>
+                        <form action="/assets/resume/Resume-Alan-Margolies.pdf" method="get" target="_blank">
+                            <button className="btn nav-link" type="submit">Resume<span className="sr-only">(current)</span></button>
+                        </form>
                     </li>
                 </ul>
             </div>
-            <div>{state.pageName}</div>
         </nav>
     )
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,12 +1,11 @@
-import React from "react";
 import About from "../pages/About";
 import Portfolio from "../pages/Portfolio";
 import Contact from "../pages/Contact";
 import Resume from "../pages/Resume";
-import { UseContext, usePageContext } from "../utils/GlobalState";
+import { usePageContext } from "../utils/GlobalState";
 
 function Page() {
-    const [state, dispatch] = usePageContext();
+    const [state] = usePageContext();
     switch (state.pageName) {
         case "about": return About();
         case "portfolio": return Portfolio();
