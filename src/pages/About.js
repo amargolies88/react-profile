95d00434d0c187fa2db825e7294e0dd13ec5601b
Alan Margolies <amargolies88@gmail.com>
2021-03-10 16:33:57 -0500

    fix background image with correct path

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,7 +16,7 @@ function About() {
                 <div className="col mx-3 py-3 lh-2">
                     <p>
                         <img className="img-fluid float-left mr-3 mb-2" src="/react-profile/alan_suit_200_150.png" alt="pic of alan" />
-                    Alan Margolies was born in 1988 in Metairie, Louisiana. His family moved to Lawrenceville, Georgia in 1990 where he spent most of the rest of his life. Alan is an experienced guitar teacher looking for a big change in career. Alan built his first website in 2004 where he first learned how to use software like Dreamweaver, Photoshop, and Flash. After a long while of working in retail stores and trying to maintain a minimum student base, in 2019 Alan recalled his inspiration from fifteen years ago and enrolled in the Full Stack Web Developer course at Georgia Tech's Coding Bootcamp. There with the helpful guidence of expert teachers he hopes to become an excellent full stack developer.
+                    Born in Louisiana and raised in Georgia, Alan is a formor guitar teacher seeking a career in web development. He built his first website at the young age of 14 playing with software like Dreamweaver, Photoshop, and Flash. As a graduate of GSU and GT Coding Bootcamp, Alan seeks to bring his experience and talents into the future, the world of programming.
                 </p>
                 </div>
             </div>
